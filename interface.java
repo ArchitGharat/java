ace9fd8a457cd2d7df0f32796ab07901446dded7
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-12-06 03:24:16 -0800

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,27 @@
+Interface Abc
+{
+    static final double pi=3.14;
+    public void disp();
+}
+class def
+{ 
+    int i=22;
+    void disp1()
+    {
+        system.out.println("in disp:");
+    }
+}
+class DemoInnterface extends Defimplements Abc
+{
+    public void disp()
+    {
+        system.out.println("In disp:");
+    }
+    public static void main 9string[] args);
+    {
+        DemoInterface di=n+..  DemoInterface();
+        di.display();
+        di.disp();
+    }
+}
+    
