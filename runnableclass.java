06bc2af3709394442ad91cfbdb93437d31fc73d7
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-12-06 03:26:05 -0800

    Create runnableclass.java

---
+++ runnableclass.java
@@ -0,0 +1,14 @@
+public class exampleclass implement runnable
+
+@override
+public class run()
+{
+    system.out.println("inread has ended");
+}
+public static void main(string)[] args)
+{
+    exampleclass ex + new ExampleCclass();
+    Thread t1= new ExampleClass();
+    t1.start();
+    System.out.println("hello");
+}
